95848d1559ac8781ce24f0433d6a7cad3d389d3a
Jonathan Heathcote <mail@jhnet.co.uk>
2019-10-18 07:55:00 +0100

    Update screenshots.

--- src/images.js
+++ src/images.js
@@ -1,25 +1,30 @@
 /**
- * All images loaded as Javascript data URIs.
+ * All images.
  */
 
-import fs from "fs";
 
-/**
- * Convert a buffer into a data URL
- */
-function bufferToURL(buffer) {
-	return `data:image/svg+xml;base64,${buffer.toString('base64')}`;
+import logoWithText from "./images/logo_with_text.svg";
+
+import printerIcon from "./images/printer.svg";
+import instructionsStep1 from "./images/step_1.svg";
+import instructionsStep2 from "./images/step_2.svg";
+import instructionsStep3 from "./images/step_3.svg";
+
+import examplePads from "./images/examplePads.svg";
+import upIcon from "./images/up.svg";
+import downIcon from "./images/down.svg";
+import deleteIcon from "./images/delete.svg";
+import addIcon from "./images/add.svg";
+
+export {
+	logoWithText,
+	printerIcon,
+	instructionsStep1,
+	instructionsStep2,
+	instructionsStep3,
+	examplePads,
+	upIcon,
+	downIcon,
+	deleteIcon,
+	addIcon,
 }
-
-export const logoWithText = bufferToURL(fs.readFileSync(__dirname + "/images/logo_with_text.svg"));
-
-export const printerIcon = bufferToURL(fs.readFileSync(__dirname + "/images/printer.svg"));
-export const instructionsStep1 = bufferToURL(fs.readFileSync(__dirname + "/images/step_1.svg"));
-export const instructionsStep2 = bufferToURL(fs.readFileSync(__dirname + "/images/step_2.svg"));
-export const instructionsStep3 = bufferToURL(fs.readFileSync(__dirname + "/images/step_3.svg"));
-
-export const examplePads = bufferToURL(fs.readFileSync(__dirname + "/images/examplePads.svg"));
-export const upIcon = bufferToURL(fs.readFileSync(__dirname + "/images/up.svg"));
-export const downIcon = bufferToURL(fs.readFileSync(__dirname + "/images/down.svg"));
-export const deleteIcon = bufferToURL(fs.readFileSync(__dirname + "/images/delete.svg"));
-export const addIcon = bufferToURL(fs.readFileSync(__dirname + "/images/add.svg"));
